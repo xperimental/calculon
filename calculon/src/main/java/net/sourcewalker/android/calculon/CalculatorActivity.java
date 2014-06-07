9128c29067229932ac7cfb430f92fcf1bf608eea
Robert Jacob <xperimental@solidproject.de>
2014-06-07 15:32:07 +0200

    Added empty activity.

---
+++ calculon/src/main/java/net/sourcewalker/android/calculon/CalculatorActivity.java
@@ -0,0 +1,14 @@
+package net.sourcewalker.android.calculon;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class CalculatorActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+
+}
