a5e140582733b279056cbce47f86b426272c5049
Robert Jacob <xperimental@solidproject.de>
2014-06-09 00:43:18 +0200

    Implemented frontend.

--- calculon/src/main/java/net/sourcewalker/android/calculon/CalculatorActivity.java
+++ calculon/src/main/java/net/sourcewalker/android/calculon/CalculatorActivity.java
@@ -7,12 +7,51 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import net.sourcewalker.android.calculon.client.CalcServerRequest;
+import net.sourcewalker.android.calculon.client.RequestData;
+import net.sourcewalker.android.calculon.client.ResponseData;
+
 public class CalculatorActivity extends FragmentActivity {
 
     private static final int OPERATOR_NONE = 0;
     private static final int OPERATOR_PLUS = 1;
     private static final int OPERATOR_MINUS = 2;
+    private static final int OPERATOR_MULTIPLY = 3;
+    private static final int OPERATOR_DIVIDE = 4;
 
+    private static final int[] BUTTONS = new int[] {
+            R.id.number_0,
+            R.id.number_1,
+            R.id.number_2,
+            R.id.number_3,
+            R.id.number_4,
+            R.id.number_5,
+            R.id.number_6,
+            R.id.number_7,
+            R.id.number_8,
+            R.id.number_9,
+            R.id.plus,
+            R.id.minus,
+            R.id.multiply,
+            R.id.divide,
+            R.id.enter
+    };
+
+    private static final RequestQueue.RequestFilter ALL_REQUESTS = new RequestQueue.RequestFilter() {
+        @Override
+        public boolean apply(Request<?> request) {
+            return true;
+        }
+    };
+
+    private RequestQueue requestQueue;
+    private RequestListener requestListener;
     private EditText inputView;
     private int currentOperator = OPERATOR_NONE;
     private int savedValue = 0;
@@ -23,7 +62,18 @@ public class CalculatorActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        requestQueue = Volley.newRequestQueue(this);
+        requestListener = new RequestListener();
+
         inputView = (EditText) findViewById(R.id.input);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Cancel all pending requests
+        requestQueue.cancelAll(ALL_REQUESTS);
     }
 
     public void onNumberPressed(View view) {
@@ -40,6 +90,8 @@ public class CalculatorActivity extends FragmentActivity {
     }
 
     public void onClearPressed(View view) {
+        requestQueue.cancelAll(ALL_REQUESTS);
+        setUiEnabled(true);
         inputView.setText("0");
         currentOperator = OPERATOR_NONE;
         savedValue = 0;
@@ -55,8 +107,10 @@ public class CalculatorActivity extends FragmentActivity {
                 setOperator(OPERATOR_MINUS);
                 break;
             case R.id.multiply:
+                setOperator(OPERATOR_MULTIPLY);
+                break;
             case R.id.divide:
-                Log.d(Constants.TAG, "Currently not implemented!");
+                setOperator(OPERATOR_DIVIDE);
                 break;
         }
     }
@@ -86,7 +140,28 @@ public class CalculatorActivity extends FragmentActivity {
                 savedValue = savedValue - inputValue;
                 inputView.setText(Integer.toString(savedValue));
                 break;
+            case OPERATOR_MULTIPLY:
+            case OPERATOR_DIVIDE:
+                onlineCalculation(currentOperator, savedValue, inputValue);
+                break;
         }
+    }
+
+    private void onlineCalculation(int operation, int opOne, int opTwo) {
+        RequestData requestData = new RequestData();
+        switch (operation) {
+            case OPERATOR_MULTIPLY:
+                requestData.setOperation("MULTIPLY");
+                break;
+            case OPERATOR_DIVIDE:
+                requestData.setOperation("DIVIDE");
+                break;
+        }
+        requestData.setOperandOne(opOne);
+        requestData.setOperandTwo(opTwo);
+        setStatusMessage("WAIT");
+        CalcServerRequest request = new CalcServerRequest(requestData, requestListener);
+        requestQueue.add(request);
     }
 
     public int getInputValue() {
@@ -97,6 +172,32 @@ public class CalculatorActivity extends FragmentActivity {
             Log.e(Constants.TAG, "Invalid number: " + input);
             return 0;
         }
+    }
+
+    private void setStatusMessage(String message) {
+        inputView.setText(message);
+        setUiEnabled(false);
+    }
+
+    private void setUiEnabled(boolean enabled) {
+        for (int id : BUTTONS) {
+            findViewById(id).setEnabled(enabled);
+        }
+    }
+
+    private final class RequestListener implements Response.Listener<ResponseData>, Response.ErrorListener {
+
+        @Override
+        public void onResponse(ResponseData response) {
+            inputView.setText(Integer.toString(response.getResult()));
+            setUiEnabled(true);
+        }
+
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            setStatusMessage("ERR!");
+        }
+
     }
 
 }
